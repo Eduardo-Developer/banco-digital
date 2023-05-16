1fb572d993d1425db5947afc3c79fae6296d4c26
Cadu <carlos.souza@maximatech.com.br>
2023-05-16 18:02:24 -0300

    create extension file and layout adjustments

---
+++ app/src/main/java/com/edudev/bancodigital/util/Extensions.kt
@@ -0,0 +1,15 @@
+package com.edudev.bancodigital.util
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import com.edudev.bancodigital.R
+
+fun Fragment.initToolbar(toolbar: Toolbar, homeAsUpEnabled: Boolean = true) {
+    (activity as AppCompatActivity).setSupportActionBar(toolbar)
+    (activity as AppCompatActivity).title = ""
+    (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(homeAsUpEnabled)
+    (activity as AppCompatActivity).supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
+    toolbar.setNavigationOnClickListener { activity?.onBackPressed() }
+
+}
